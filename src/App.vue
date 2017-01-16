8c9529ac3ca8e2b6962f9c41b7552a8779d2598c
Sergey Panfilov <se-panfilov@ya.ru>
2017-01-16 16:16:12 +0100

    infrastructure update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    Some
+    <next-race></next-race>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+  import NextRace from './components/next-race'
 
-export default {
-  name: 'app',
-  components: {
-    Hello
+  export default {
+    name: 'app',
+    data () {
+      return {
+        text: '',
+        category: null,
+        vendor: null
+      }
+    },
+    methods: {},
+    components: {
+      NextRace
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  html, body
+    margin 0
+    padding 0
+    height 100%
+
+  body
+    background-color #FFF
+    color #3c3c3c
+    font-family 'Roboto', sans-serif
+    font-weight 500
+    font-size 16px
+    *
+      font-family 'Roboto', sans-serif
+      font-weight 500
+
+  input[type="search"]
+    -moz-appearance textfield
+    -webkit-appearance textfield
+    appearance textfield
+    -webkit-box-sizing border-box
+    -moz-box-sizing border-box
+    box-sizing border-box
+
+  article, aside, details, figcaption, figure,
+  footer, header, menu, nav, section, canvas
+    display block
+
 </style>
---
+++ src/components/next-race.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class='race-block'>
+    <div class="race-block__title title">
+      <div class="title__info -heavy"></div>
+      <div class="title__info -right"></div>
+    </div>
+    <div class="race-block__subtitle">
+      <div class="subtitle__info"></div>
+      <div class="subtitle__info"></div>
+      <div class="subtitle__info"></div>
+      <div class="subtitle__info--img"></div>
+    </div>
+    <div class="race-block__info">
+      <div class="info__cell -img"></div>
+      <div class="info__cell"></div>
+      <div class="info__cell -btn"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NextRace',
+    data () {
+      return {
+      }
+    },
+    props: {
+      filter: {
+        type: String
+      }
+    },
+    methods: {
+      some (some) {
+      }
+    },
+//    computed: {
+//      filteredGames () {
+//        return this.games
+//          .filter(v => {
+//            if (!this.category) return true
+//            return v.gameType === this.category
+//          })
+//          .filter(v => {
+//            if (!this.vendor) return true
+//            return v.vendor === this.vendor
+//          })
+//          .filter(v => {
+//            if (!this.text && this.text.length === 0) return true
+//            return v.name.toLowerCase().indexOf(this.text.toLowerCase()) !== -1
+//          })
+//      }
+//    },
+    components: {
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  text_color = #464646
+
+
+</style>
