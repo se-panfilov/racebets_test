1ad35f8f6367bff28b4a94260d05e7ea6c9ec576
Sergey Panfilov <se-panfilov@ya.ru>
2017-01-16 21:42:11 +0100

    fixes

--- src/components/next-race.vue
+++ src/components/next-race.vue
@@ -3,21 +3,33 @@
     <div class="race-block__title title">
       <div class="title__info -heavy"></div>
       <div class="title__info -right"></div>
+      <span>Upcoming:</span>
+      <span v-text="upcomingRace.race_type"></span>
+      <span v-text="upcomingRace.purse.amount"></span>
+      <span v-text="upcomingRace.post_time"></span>
+      <div>-----</div>
     </div>
-    <div class="race-block__subtitle" v-for="r in selectedRaces">
+    <div class="race-block__subtitle" v-for="race in selectedRaces">
       <div class="subtitle__info">
-        <span v-text="r.race_type"></span>
-        <span v-text="r.purse.amount"></span>
+        <span v-text="race.race_type"></span>
+        <span v-text="race.purse.amount"></span>
+        <span v-text="race.post_time"></span>
       </div>
       <div class="subtitle__info"></div>
       <div class="subtitle__info"></div>
       <div class="subtitle__info--img"></div>
+      <ul class="race-block__runners" v-for="runner in race.runners">
+        <li class="runners__cell" v-show="runner.silk">
+          <a v-bind:href="getSilkUrl(runner.id_race)">
+            <img v-bind:src="getSilkImg(runner.silk)" v-bind:alt="runner.silk">
+          </a>
+        </li>
+        <li class="runners__cell" v-text="runner.name"></li>
+        <li class="runners__cell">
+          <button type="button" v-text="runner.odds" @click="onOddsClick()"></button>
+        </li>
+      </ul>
     </div>
-    <ul class="race-block__runners">
-      <li class="runners__cell -img"></li>
-      <li class="runners__cell"></li>
-      <li class="runners__cell"></li>
-    </ul>
     <button @click="debug">Debug</button>
     <span v-text="selectedRaces.length"></span>
   </div>
@@ -43,6 +55,15 @@
       debug () {
 //        console.info(this.races)
 //        console.log(this.selectedRaces)
+      },
+      onOddsClick () {
+
+      },
+      getSilkUrl (id) {
+        return `http://www.racebets.com/bet/${id}` // https?
+      },
+      getSilkImg (img) {
+        return `static/assets/silks/${img}`
       }
     },
     computed: {
@@ -54,6 +75,13 @@
         return arr.filter(v => {
           return filters.includes(v.race_type)
         })
+      },
+      upcomingRace () {
+        const sortedRaces = this.selectedRaces.sort((a, b) => a.post_time - b.post_time)
+
+        return sortedRaces.length > 0 ? sortedRaces[0] : { purse: {} }
+//        return this.selectedRaces().sort((a, b) => a.post_time - b.post_time)
+//        return this.races().sort((a, b) => a.post_time - b.post_time)
       }
     },
     components: {
